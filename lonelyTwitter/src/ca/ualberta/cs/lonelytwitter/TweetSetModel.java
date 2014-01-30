4f30cfa109e6ddfae21f338768cb0fc35b4293e6
Alex <alexgfh@gmail.com>
2014-01-30 15:42:52 -0700

    test commi

---
+++ lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/TweetSetModel.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.lonelytwitter;
+
+public class TweetSetModel {
+
+	private int count = 0;
+	
+	public int countTweets() {
+		return 0;
+	}
+
+	public void addTweet(NormalTweetModel normalTweetModel) {
+		count++;
+	}
+
+}
